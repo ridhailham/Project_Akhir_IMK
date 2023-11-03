d870a909464d15bbdcb81e0da8a140309eaa3bbd
Ridha Ilham Adi Setyawan <117886100+ridhailham@users.noreply.github.com>
2023-11-03 22:13:34 +0700

    update basket & silat

--- frontend/src/HomeComponent/Landing.jsx
+++ frontend/src/HomeComponent/Landing.jsx
@@ -4,12 +4,12 @@ const Landing = () => {
   return (
     <>
       <div className="absolute top-0 h-fit w-full flex items-center justify-center">
-        <img src="./Landing.svg" alt="" className="w-full" />
+        <img src="./Landing1.jpeg" alt="" className="w-full" />
       </div>
 
       <Parallax speed={10} className="top-0">
         <div className="pt-[20%] pb-[14%] md:pt-[15vw] md:pb-[1vw] h-fit xl:mb-[-10vw] xl:pt-[11vw] w-full flex items-center justify-center">
-          <img src="./BTEEHero.svg" alt="" className=" w-full px-[10vw]" />
+          <img src="./pdi5.jpg" alt="" className="px-[10vw] bg-transparent" />
         </div>
       </Parallax>
     </>
